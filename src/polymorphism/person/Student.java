a9bba7b2ad4c3fffdda6015cd3a466e7d2729579
frnmst/Franco Masotti <franco.masotti@live.com>
2016-05-12 19:04:52 +0200

    Added missing license headers.

--- src/polymorphism/person/Student.java
+++ src/polymorphism/person/Student.java
@@ -1,3 +1,12 @@
+/*
+ * Student.java
+ *
+ * Copyright © 2016 Franco Masotti <franco.masotti@student.unife.it>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the LICENSE file for more details.
+ */
+
 package person;
 
 public class Student extends Person
